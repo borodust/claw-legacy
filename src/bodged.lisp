637545fe7fcbbd7711fefc1ce54d8f406eb7389f
Pavel Korolev <dev@borodust.org>
2018-10-08 08:18:04 +0300

    Implicitly define claw definition package

--- src/bodged.lisp
+++ src/bodged.lisp
@@ -29,33 +29,39 @@
                        (windows-environment "gnu")
                        (local-only nil) language standard)
   (declare (ignore body))
-  `(progn
-     (%c-include
-      ',(list system-name header)
-      :spec-path ',(list system-name spec-module)
-      :definition-package ,in-package
-      :local-environment #+windows ,windows-environment
-      #-windows "gnu"
-      :local-only ,(or *local-only* local-only)
-      :include-arch ("x86_64-pc-linux-gnu"
-                     "i686-pc-linux-gnu"
-                     ,(string+ "x86_64-pc-windows-" windows-environment)
-                     ,(string+ "i686-pc-windows-" windows-environment)
-                     "x86_64-apple-darwin-gnu"
-                     "i686-apple-darwin-gnu")
-      :sysincludes ',(append (parse-sysincludes system-name sysincludes)
-                             (dump-all-gcc-include-paths))
-      :includes ',(parse-sysincludes system-name includes)
-      :include-sources ,include-sources
-      :include-definitions ,include-definitions
-      :exclude-sources ,exclude-sources
-      :exclude-definitions ,exclude-definitions
-      :filter-spec-p t
-      :language ,language
-      :standard ,standard
-      :symbol-regex ,rename-symbols)
-     (defmethod dump-c-wrapper ((package-name (eql ,in-package)) wrapper-path)
-       (declare (ignore package-name))
-       (write-c-library-implementation wrapper-path
-                                       ,header
-                                       (package-functions ,in-package)))))
+  (destructuring-bind (in-package &rest nicknames) (ensure-list in-package)
+    (unless in-package
+      (error ":in-package must be supplied"))
+    `(progn
+       (uiop:define-package ,in-package
+           (:nicknames ,@nicknames)
+         (:use))
+       (%c-include
+        ',(list system-name header)
+        :spec-path ',(list system-name spec-module)
+        :definition-package ,in-package
+        :local-environment #+windows ,windows-environment
+        #-windows "gnu"
+        :local-only ,(or *local-only* local-only)
+        :include-arch ("x86_64-pc-linux-gnu"
+                       "i686-pc-linux-gnu"
+                       ,(string+ "x86_64-pc-windows-" windows-environment)
+                       ,(string+ "i686-pc-windows-" windows-environment)
+                       "x86_64-apple-darwin-gnu"
+                       "i686-apple-darwin-gnu")
+        :sysincludes ',(append (parse-sysincludes system-name sysincludes)
+                               (dump-all-gcc-include-paths))
+        :includes ',(parse-sysincludes system-name includes)
+        :include-sources ,include-sources
+        :include-definitions ,include-definitions
+        :exclude-sources ,exclude-sources
+        :exclude-definitions ,exclude-definitions
+        :filter-spec-p t
+        :language ,language
+        :standard ,standard
+        :symbol-regex ,rename-symbols)
+       (defmethod dump-c-wrapper ((package-name (eql ,in-package)) wrapper-path)
+         (declare (ignore package-name))
+         (write-c-library-implementation wrapper-path
+                                         ,header
+                                         (package-functions ,in-package))))))
